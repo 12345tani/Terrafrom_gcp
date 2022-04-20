7728c048d9ea4cf3315b663fd3bffedca47a1d03
12345tani <engineertaniya@gmail.com>
2022-04-20 19:40:30 +0530

    Update main.tf

--- modules/ALB/main.tf
+++ modules/ALB/main.tf
@@ -1,13 +1,13 @@
 provider "google" {
 #version = "3.5.0"
-credentials = file("E:\\project_GCP_Terrform\\accesskey\\tf-dev-gcp-311d4b2691b9.json")
+credentials = file("../../accesskey/tf-dev-gcp-311d4b2691b9.json")
 project = "tf-dev-gcp"
 region = "us-central1"
 zone = "us-central1-c"
 }
 provider "google-beta" {
 #version = "3.5.0"
-credentials = file("E:\\project_GCP_Terrform\\accesskey\\tf-dev-gcp-311d4b2691b9.json")
+credentials = file("../../accesskey/tf-dev-gcp-311d4b2691b9.json")
 project = "tf-dev-gcp"
 region = "us-central1"
 zone = "us-central1-c"
