308c3fd2d52fdb0e2f8249b46cb29179a2df2666
12345tani <engineertaniya@gmail.com>
2022-05-01 17:36:14 +0530

    Update main.tf

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -20,14 +20,14 @@ locals {
 provider "google" {
 #version = "3.5.0"
 #credentials = file("../../accesskey/tf-dev-gcp-311d4b2691b9.json")
-project = "google-gcp-test"
+project = "sacred-highway-348910"
 region = "us-central1"
 zone = "us-central1-c"
 }
 provider "google-beta" {
 #version = "3.5.0"
 #credentials = file("../../accesskey/tf-dev-gcp-311d4b2691b9.json")
-project = "google-gcp-test"
+project = "sacred-highway-348910"
 region = "us-central1"
 zone = "us-central1-c"
 }
