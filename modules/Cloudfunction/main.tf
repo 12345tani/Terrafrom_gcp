a485d46fa743204cc920f238855e276c34fa21f8
12345tani <engineertaniya@gmail.com>
2022-05-03 15:37:06 +0530

    Update main.tf

--- modules/Cloudfunction/main.tf
+++ modules/Cloudfunction/main.tf
@@ -1,7 +1,7 @@
 provider "google" {
 #version = "3.5.0"
 #credentials = file("../../accesskey/tf-dev-gcp-311d4b2691b9.json")
-project = "tf-dev-gcp"
+project = "sacred-highway-348910"
 region = "us-central1"
 zone = "us-central1-c"
 }
