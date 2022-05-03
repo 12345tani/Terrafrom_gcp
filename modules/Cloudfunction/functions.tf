bf4429aab417d009579ae48dbf9ff4d32177381b
12345tani <engineertaniya@gmail.com>
2022-05-03 15:42:53 +0530

    Update functions.tf

--- modules/Cloudfunction/functions.tf
+++ modules/Cloudfunction/functions.tf
@@ -1,6 +1,6 @@
 # Generates an archive of the source code compressed as a .zip file.
 locals {
-  root_dir = " ../../modules/Cloudfunction/src"
+  root_dir = " ./src"
 }
 data "archive_file" "source" {
   type        = "zip"
