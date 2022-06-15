df8d158930f43844f6ec4b0c8e69800a293f6720
12345tani <engineertaniya@gmail.com>
2022-06-15 18:24:47 +0530

    Update main.tf

--- modules/CloudSQL/main.tf
+++ modules/CloudSQL/main.tf
@@ -1,7 +1,7 @@
 #variable "project" {}
 provider "google" {
 #version = "3.5.0"
-credentials = file("E:\\project_GCP_Terrform\\accesskey\\tf-dev-gcp-311d4b2691b9.json")
+#credentials = file("E:\\project_GCP_Terrform\\accesskey\\tf-dev-gcp-311d4b2691b9.json")
 project = "tf-dev-gcp"
 region = "us-central1"
 zone = "us-central1-c"
